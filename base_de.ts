bb69c8d28f852ffc4c36fa44983ffec76646ae60
Oliver Köhler <olk.git@posteo.eu>
2023-03-19 09:48:10 +0100

    translation template

---
+++ base_de.ts
@@ -0,0 +1,225 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="de_DE">
+<context>
+    <name>ConfirmDeletionDialog</name>
+    <message>
+        <location filename="views/confirmationDialogs.py" line="35"/>
+        <source>Delete selected item?</source>
+        <translation>Ausgewählten Eintrag löschen?</translation>
+    </message>
+</context>
+<context>
+    <name>ConfirmRestartDialog</name>
+    <message>
+        <location filename="views/confirmationDialogs.py" line="21"/>
+        <source>Changes applied require a restart. Restart now?</source>
+        <translation>Vorgenommene Änderungen erfordern einen Neustart. Jetzt neustarten?</translation>
+    </message>
+</context>
+<context>
+    <name>ConfirmScheduleUpdateDialog</name>
+    <message>
+        <location filename="views/confirmationDialogs.py" line="28"/>
+        <source>Write changes to database?</source>
+        <translation>Änderungen in die Datenbank schreiben?</translation>
+    </message>
+</context>
+<context>
+    <name>ConfirmationDialog</name>
+    <message>
+        <location filename="views/confirmationDialogs.py" line="11"/>
+        <source>Yes</source>
+        <translation>Ja</translation>
+    </message>
+    <message>
+        <location filename="views/confirmationDialogs.py" line="12"/>
+        <source>No</source>
+        <translation>Nein</translation>
+    </message>
+</context>
+<context>
+    <name>EditorDialog</name>
+    <message>
+        <location filename="views/base_classes.py" line="36"/>
+        <source>OK</source>
+        <translation>OK</translation>
+    </message>
+    <message>
+        <location filename="views/base_classes.py" line="37"/>
+        <source>Cancel</source>
+        <translation>Abbrechen</translation>
+    </message>
+</context>
+<context>
+    <name>EditorWidget</name>
+    <message>
+        <location filename="views/base_classes.py" line="70"/>
+        <source>OK</source>
+        <translation>OK</translation>
+    </message>
+    <message>
+        <location filename="views/base_classes.py" line="71"/>
+        <source>Cancel</source>
+        <translation>Abbrechen</translation>
+    </message>
+</context>
+<context>
+    <name>Form</name>
+    <message>
+        <location filename="ui/optionsEditor.ui" line="14"/>
+        <source>Form</source>
+        <translation>Formular</translation>
+    </message>
+    <message>
+        <location filename="ui/optionsEditor.ui" line="29"/>
+        <source>Options</source>
+        <translation>Optionen</translation>
+    </message>
+    <message>
+        <location filename="ui/optionsEditor.ui" line="38"/>
+        <source>Locale</source>
+        <translation>Sprache</translation>
+    </message>
+    <message>
+        <location filename="ui/optionsEditor.ui" line="48"/>
+        <source>Theme</source>
+        <translation>Theme</translation>
+    </message>
+</context>
+<context>
+    <name>MonthList</name>
+    <message>
+        <location filename="views/lists.py" line="8"/>
+        <source>January</source>
+        <translation>Januar</translation>
+    </message>
+    <message>
+        <location filename="views/lists.py" line="9"/>
+        <source>February</source>
+        <translation>Februar</translation>
+    </message>
+    <message>
+        <location filename="views/lists.py" line="10"/>
+        <source>March</source>
+        <translation>März</translation>
+    </message>
+    <message>
+        <location filename="views/lists.py" line="11"/>
+        <source>April</source>
+        <translation>April</translation>
+    </message>
+    <message>
+        <location filename="views/lists.py" line="12"/>
+        <source>May</source>
+        <translation>Mai</translation>
+    </message>
+    <message>
+        <location filename="views/lists.py" line="13"/>
+        <source>June</source>
+        <translation>Juni</translation>
+    </message>
+    <message>
+        <location filename="views/lists.py" line="14"/>
+        <source>July</source>
+        <translation>Juli</translation>
+    </message>
+    <message>
+        <location filename="views/lists.py" line="15"/>
+        <source>August</source>
+        <translation>August</translation>
+    </message>
+    <message>
+        <location filename="views/lists.py" line="16"/>
+        <source>September</source>
+        <translation>September</translation>
+    </message>
+    <message>
+        <location filename="views/lists.py" line="17"/>
+        <source>October</source>
+        <translation>Oktober</translation>
+    </message>
+    <message>
+        <location filename="views/lists.py" line="18"/>
+        <source>November</source>
+        <translation>November</translation>
+    </message>
+    <message>
+        <location filename="views/lists.py" line="21"/>
+        <source>December</source>
+        <translation>Dezember</translation>
+    </message>
+</context>
+<context>
+    <name>PersonModel</name>
+    <message>
+        <location filename="logic/table_models.py" line="19"/>
+        <source>First Name</source>
+        <translation>Vorname</translation>
+    </message>
+    <message>
+        <location filename="logic/table_models.py" line="20"/>
+        <source>Last Name</source>
+        <translation>Nachname</translation>
+    </message>
+    <message>
+        <location filename="logic/table_models.py" line="21"/>
+        <source>E-Mail</source>
+        <translation>E-Mail</translation>
+    </message>
+</context>
+<context>
+    <name>Properties</name>
+    <message>
+        <location filename="logic/config.py" line="21"/>
+        <source>dark</source>
+        <translation>dunkel</translation>
+    </message>
+    <message>
+        <location filename="logic/config.py" line="22"/>
+        <source>light</source>
+        <translation>hell</translation>
+    </message>
+</context>
+<context>
+    <name>mainDialog</name>
+    <message>
+        <location filename="ui/main.ui" line="48"/>
+        <source>New Database ...</source>
+        <translation>Neue Datenbank ...</translation>
+    </message>
+    <message>
+        <location filename="ui/main.ui" line="61"/>
+        <source>Load Database ...</source>
+        <translation>Datenbank laden ...</translation>
+    </message>
+    <message>
+        <location filename="ui/main.ui" line="74"/>
+        <source>Options</source>
+        <translation>Optionen</translation>
+    </message>
+</context>
+<context>
+    <name>personView</name>
+    <message>
+        <location filename="ui/tableView.ui" line="20"/>
+        <source>Form</source>
+        <translation>Formular</translation>
+    </message>
+    <message>
+        <location filename="ui/tableView.ui" line="28"/>
+        <source>Search ...</source>
+        <translation>Suche ...</translation>
+    </message>
+    <message>
+        <location filename="ui/tableView.ui" line="43"/>
+        <source>Add</source>
+        <translation>Hinzufügen</translation>
+    </message>
+    <message>
+        <location filename="ui/tableView.ui" line="56"/>
+        <source>Delete</source>
+        <translation>Löschen</translation>
+    </message>
+</context>
+</TS>
